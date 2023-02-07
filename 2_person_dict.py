57c5a4c7fe4088c0b7a90b935bc4ec0bc26e0fda
NhuPham <you@example.com>
2023-02-06 22:35:51 -0600

    eq_dict

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -4,4 +4,33 @@ person["lname"] = "Fonebone"
 person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
-person["pets"] = {"dog": "Fido", "cat": "Sox"}
+person["pets"] = {"dog": ["Fido", "Bruce"], "cat": "Sox"}
+"""
+print(person)
+
+# print out the name of the spouse
+print(person["spouse"])
+
+# print our the name of the second child
+print(f"the name of the second child is: {person['children'][1]}")
+
+
+# print out the name of the cat
+print(person["pets"]["cat"])
+
+# use a loop to print out the names of each child
+for x in person["children"]:
+    print(x)
+"""
+# use a loop to print out the pets in the following format:
+# the type of pet is: dog and the name of the pet is: Fido
+pet_dict = person["pets"]
+for key, value in pet_dict.items():
+    print(f"the type of pet is: {key} and the name of the pet is: {value[0]}")
+
+# the result will be Bruce and o for cat's name because this dog's name is a list
+# and cat's name is a string.
+# we will need another coding thingy to resolve this
+
+# string = "Sox"
+# print(string)
--- 7_schools_report.py
+++ 7_schools_report.py
@@ -17,3 +17,24 @@ Display report for all universities that have a total price for in-state student
 
 
 """
+
+import json
+
+infile = open("school_data.json", "r")
+
+schools = json.load(infile)
+conf_schools = [372, 108, 107, 130]
+print(type(schools))
+
+# how many schools are in the file?
+
+print(len(schools))
+
+for school in schools:
+    if school["NCAA"]["NAIA conference number football (IC2020)"] in conf_schools:
+        if school["Graduation rate  women (DRVGR2020)"] > 75:
+            print(f"Name of University: {school['instnm']}")
+            print(
+                f"Graduation rate for women: {school['Graduation rate  women (DRVGR2020)']}"
+            )
+            print()
